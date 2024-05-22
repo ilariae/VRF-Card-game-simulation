c5c55ed725d1b8bb02252b6c5d9a89ccaafc4ac2
Thomas Liu <tommyliu9@gmail.com>
2024-05-22 17:24:09 +0800

    fix: breaking changes

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,56 @@ use schnorrkel::Keypair;
 struct GameState {}
 
 struct Player {
+    id: u8,
     key_pair: Keypair,
-    selected_card: u32,
-    comittment: u32,
+    selected_card: Option<u32>,
+    commitment: Option<u32>,
 }
 
 fn create_player(player_number: u8) -> Player {
     // TODO: generate a private / public key pair
-    todo!()
+
+    return Player {
+        id: player_number,
+        key_pair: schnorrkel::Keypair::generate(),
+        selected_card: None,
+        commitment: None,
+    };
 }
 
 fn generate_random_for_player(player: &Player) {
-    // TODO: update selected_card and generate proof / committment
-
+    // TODO: update selected_card and generate proof / commitments
     todo!()
 }
 
-fn verify_committments(player_committments: Vec<Player>) {
-    // TODO: verify the proof /committments of the players
+fn verify_commitments(player_commitments: Vec<Player>) {
+    // TODO: verify the proof /commitments of the players
     todo!()
 }
 
 fn select_winner(players: Vec<Player>) {
-    // TODO: verify the proof /committments of the players
+    // TODO: verify the proof /commitments of the players
     todo!()
 }
 
 fn main() {
-    let num_players = 3;
+    // GAME VARIABLES
+    let NUM_PLAYERS = 3;
 
     while true {
+        let mut players = Vec::new();
 
-        // 1) create player key pair
+        for i in 0..NUM_PLAYERS {
+            // 1) create player key pair
 
-        // 2) each player generates a random values and publishes the comittment
+            let player = create_player(i as u8);
+            // 2) each player generates a random values and publishes the commitment
 
-        // 3) system reveal the committment
+            generate_random_for_player(&player);
+            players.push(player);
+        }
+
+        // 3) system reveal the commitment
 
         // 4) people publish their original random values
 
