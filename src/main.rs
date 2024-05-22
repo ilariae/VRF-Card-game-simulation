5318d42b1761dcd2bfa0403f545553d7f4f1f6f6
Thomas Liu <tommyliu9@gmail.com>
2024-05-22 19:18:24 +0800

    fixed stuff

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use rand::{rngs::OsRng, RngCore};
 use schnorrkel::{context::SigningContext, signing_context, Keypair};
 use sha2::{Digest, Sha256, Sha512};
 
-const CARD_GAME_SIGNING_CONTEXT: SigningContext =
-    signing_context(String::from("Card Game").as_bytes());
+const CARD_GAME_SIGNING_CONTEXT: &str = "Card Game";
+const NUM_PLAYERS: u8 = 3;
 
 #[derive(Debug, Clone)]
 struct Player {
@@ -21,8 +21,6 @@ struct Player {
 }
 
 fn create_player(player_number: u8) -> Player {
-    // TODO: generate a private / public key pair
-
     return Player {
         id: player_number,
         key_pair: schnorrkel::Keypair::generate(),
@@ -31,6 +29,12 @@ fn create_player(player_number: u8) -> Player {
     };
 }
 
+fn generate_random_seed() {
+    let mut rng = OsRng;
+    let mut random_input = [0u8; 32]; // initializes 32 byte array filled with zeros
+    rng.fill_bytes(&mut random_input); // fills with random bytes -> random 32 bytes higher entropy
+    rng
+}
 // update selected_card and generate proof / commitments
 fn generate_random_for_player(player: &mut Player) {
     // generate random value as VRF input
@@ -38,10 +42,20 @@ fn generate_random_for_player(player: &mut Player) {
     let mut random_input = [0u8; 32]; // initializes 32 byte array filled with zeros
     rng.fill_bytes(&mut random_input); // fills with random bytes -> random 32 bytes higher entropy
 
+    // hash the vrf output to create a commitment
+    let mut hasher = Sha256::new();
+    // create new sha256 hasher instance
+
+    hasher.update(&random_input); // feeds random input to hasher
+    let commitment = hasher.finalize();
+
+    // update players selected card and commitment
+    player.commitment = Some(commitment.to_vec());
+
     // sign the input using VRF
     let vrf_result = player
         .key_pair
-        .vrf_sign(CARD_GAME_SIGNING_CONTEXT.bytes(&random_input));
+        .vrf_sign(signing_context(CARD_GAME_SIGNING_CONTEXT.as_bytes()).bytes(&random_input));
 
     // vrf_sign reduces result containing vrf output and proof
     let vrf_output = vrf_result.0; // extracts vrf output from result [0]
@@ -53,17 +67,7 @@ fn generate_random_for_player(player: &mut Player) {
     // result modulo 52 to ensure it is within the range of card values
     let card_value =
         (u64::from_le_bytes(vrf_output.as_output_bytes()[..8].try_into().unwrap()) % 52) + 1;
-
-    // hash the vrf output to create a commitment
-    let mut hasher = Sha256::new();
-    // create new sha256 hasher instance
-
-    hasher.update(&random_input); // feeds random input to hasher
-    let commitment = hasher.finalize();
-
-    // update players selected card and commitment
     player.selected_card = Some(card_value as u32);
-    player.commitment = Some(commitment.to_vec());
 }
 
 fn verify_commitments(player_commitments: Vec<Player>) {
@@ -78,25 +82,30 @@ fn select_winner(players: Vec<Player>) {
 
 fn main() {
     // GAME VARIABLES
-    let NUM_PLAYERS = 3;
     let mut continue_playing = true;
+
     while continue_playing {
         let mut players = Vec::new();
-        println!("Game will start with {} number of players", NUM_PLAYERS);
+        let game_seed = println!("Game will start with {} number of players", NUM_PLAYERS);
 
         for i in 0..NUM_PLAYERS {
             // 1) create player key pair
 
             let mut player = create_player(i as u8);
-            println!(
-                "P{} has joined the game with public_key: {:?}",
-                i,
-                player.clone().key_pair.public
-            );
+            println!("P{} has joined the game", i,);
             // 2) each player generates a random values and publishes the commitment
 
-            generate_random_for_player(&mut player);
             players.push(player);
+        }
+
+        for i in 0..NUM_PLAYERS {
+            let mut player = players[i];
+            generate_random_for_player(player);
+            println!(
+                "P{} committed value {:?}",
+                i,
+                hex::encode(player.clone().commitment.unwrap())
+            );
         }
 
         // 3) system reveal the commitment
