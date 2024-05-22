bdcfb5d7faa296ef8ac452b59d1a6c5e373a935e
Thomas Liu <tommyliu9@gmail.com>
2024-05-22 18:40:52 +0800

    fixed compilation errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use std::io::{self, Write};
+use std::{
+    clone,
+    io::{self, Read, Write},
+    ops::Shl,
+};
 
 use ::schnorrkel;
-use schnorrkel::Keypair;
+use rand::{rngs::OsRng, RngCore};
+use schnorrkel::{context::SigningContext, signing_context, Keypair};
+use sha2::{Digest, Sha256, Sha512};
 
+const CARD_GAME_SIGNING_CONTEXT: SigningContext =
+    signing_context(String::from("Card Game").as_bytes());
+
+#[derive(Debug, Clone)]
 struct Player {
     id: u8,
     key_pair: Keypair,
     selected_card: Option<u32>,
-    commitment: Option<u32>,
+    commitment: Option<Vec<u8>>,
 }
 
 fn create_player(player_number: u8) -> Player {
@@ -23,33 +33,37 @@ fn create_player(player_number: u8) -> Player {
 
 // update selected_card and generate proof / commitments
 fn generate_random_for_player(player: &mut Player) {
-    // generate random value as VRF input 
+    // generate random value as VRF input
     let mut rng = OsRng;
     let mut random_input = [0u8; 32]; // initializes 32 byte array filled with zeros
-    rng.fill_bytes(&mut random_input); // fills with random bytes -> random 32 bytes higher entropy 
+    rng.fill_bytes(&mut random_input); // fills with random bytes -> random 32 bytes higher entropy
 
     // sign the input using VRF
-    let vrf_result = player.key_pair.vrf_sign(&random_input).unwrap(); // key_pair generates vrf signature on random input
-    // vrf_sign reduces result containing vrf output and proof 
-    let vrf_output = vrf_result.0.to_output(); // extracts vrf output from result [0]
+    let vrf_result = player
+        .key_pair
+        .vrf_sign(CARD_GAME_SIGNING_CONTEXT.bytes(&random_input));
+
+    // vrf_sign reduces result containing vrf output and proof
+    let vrf_output = vrf_result.0; // extracts vrf output from result [0]
     let vrf_proof = vrf_result.1; // extracts vrf proof from reslt [1]
 
-    // calculate card value mod 52 
+    // calculate card value mod 52
     // returns the VRF output as a byte array and takes first 8 bytes of this array
     // converts the 8-byte slice into a u64 integer
     // result modulo 52 to ensure it is within the range of card values
-    let card_value = (u64::from_le_bytes(vrf_output.to_bytes()[..8].try_into().unwrap()) % 52) + 1;
+    let card_value =
+        (u64::from_le_bytes(vrf_output.as_output_bytes()[..8].try_into().unwrap()) % 52) + 1;
 
-    // hash the vrf output to create a commitment 
-    let mut hasher = Sha256::new(); // create new sha256 hasher instance
+    // hash the vrf output to create a commitment
+    let mut hasher = Sha256::new();
+    // create new sha256 hasher instance
+
     hasher.update(&random_input); // feeds random input to hasher
-    let commitment = hasher.finalize(); 
+    let commitment = hasher.finalize();
 
-    // update players selected card and commitment 
+    // update players selected card and commitment
     player.selected_card = Some(card_value as u32);
     player.commitment = Some(commitment.to_vec());
-
-    
 }
 
 fn verify_commitments(player_commitments: Vec<Player>) {
@@ -73,14 +87,15 @@ fn main() {
         for i in 0..NUM_PLAYERS {
             // 1) create player key pair
 
-            let player = create_player(i as u8);
+            let mut player = create_player(i as u8);
             println!(
                 "P{} has joined the game with public_key: {:?}",
-                i, player.key_pair.public
+                i,
+                player.clone().key_pair.public
             );
             // 2) each player generates a random values and publishes the commitment
 
-            generate_random_for_player(&player);
+            generate_random_for_player(&mut player);
             players.push(player);
         }
 
